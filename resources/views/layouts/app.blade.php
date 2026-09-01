f179057efa0ab04b71518c536ec2763f0ec7bc66
Ibrahimfaqath <ibrahimfaqath@gmail.com>
2026-09-01 10:13:50 +0700

    edit profil page in mobile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,7 +47,7 @@
     <x-navbar />
 
     <!-- MAIN CONTENT -->
-    <main class="flex-1 w-full max-w-7xl mx-auto p-4 sm:p-6 lg:p-8">
+    <main class="flex-1 w-full max-w-7xl mx-auto p-4 sm:p-6 lg:p-8 pb-24 md:pb-8">
         {{ $slot ?? '' }}
         @yield('content')
     </main>
